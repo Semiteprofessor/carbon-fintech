f50463c923375de548abc69a1cc2f55ea4ddd947
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-02 23:34:27 +0100

    Commit

--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -2,15 +2,57 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   position: absolute;
-  height: 100;
-  background-color: var(--color-grey-0);
+  height: 118px;
+  width: 100%;
+  background-color: transparent;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 `;
 
+const Left = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const Center = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  gap: 20px;
+`;
+
+const List = styled.li`
+  font-size: 14px;
+  cursor: pointer;
+  &:hover {
+    color: #007bff;
+  }
+`;
+
+const Right = styled.span`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  gap: 20px;
+`;
 const Nav = () => {
-  return <Wrapper>Nav</Wrapper>;
+  return (
+    <Wrapper>
+      <Left>Carbon</Left>
+      <Center>
+        <List>Home</List>
+        <List>Product</List>
+        <List>Contact Us</List>
+      </Center>
+      <Right>
+        <li>Sign In</li>
+        <li>Sign Up</li>
+      </Right>
+    </Wrapper>
+  );
 };
 
 export default Nav;
