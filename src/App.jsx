d5d04680dd802d86a6958474e96544f9b77341f9
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-03 07:42:02 +0100

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
       <Landing />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -1,7 +1,9 @@
+import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Wrapper = styled.nav`
   position: absolute;
+  padding: 20px 100px 20px 100px;
   top: 0;
   height: 118px;
   width: 100vw;
@@ -12,29 +14,46 @@ const Wrapper = styled.nav`
 `;
 
 const Left = styled.span`
-  font-size: 18px;
+  font-size: 36px;
   font-weight: bold;
+  color: #4300c2;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const Logo = styled.img`
+  width: 30px;
+  height: 30px;
 `;
 
 const Center = styled.ul`
   display: flex;
-  list-style: none;
+  align-items: center;
+  justify-content: center;
   margin: 0;
   padding: 0;
-  gap: 20px;
+  gap: 45px;
 `;
 
-const List = styled.li`
-  font-size: 14px;
-  cursor: pointer;
-  &:hover {
-    color: #007bff;
+const List = styled(NavLink)`
+  &.active:link,
+  &.active:visited {
+    background-color: #4300c2;
+    padding: 8px;
+    border-radius: 4px;
+    color: #fff;
   }
+  list-style: none;
+  text-decoration: none;
 `;
 
-const Right = styled.span`
+const Right = styled.ul`
   display: flex;
-  list-style: none;
+  align-items: center;
+  justify-content: center;
   margin: 0;
   padding: 0;
   gap: 20px;
@@ -42,15 +61,18 @@ const Right = styled.span`
 const Nav = () => {
   return (
     <Wrapper>
-      <Left>Carbon</Left>
+      <Left>
+        <Logo src="./images/logo.png" />
+        Carbon
+      </Left>
       <Center>
-        <List>Home</List>
-        <List>Product</List>
-        <List>Contact Us</List>
+        <List to="/">Home</List>
+        <List to="/product">Product</List>
+        <List to="/contact">Contact Us</List>
       </Center>
       <Right>
-        <li>Sign In</li>
-        <li>Sign Up</li>
+        <List to="/login">Login</List>
+        <List to="/register">Sign Up</List>
       </Right>
     </Wrapper>
   );
